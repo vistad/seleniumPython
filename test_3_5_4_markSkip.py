1e47e0494c3424cae79f2251479e00b11d7efcd6
Victor Stadnichenko <vstadnichenko@gmail.com>
2022-08-12 00:40:16 +0300

    added marks

---
+++ test_3_5_4_markSkip.py
@@ -0,0 +1,29 @@
+# @pytest.mark.skip decoration is used to skip certain tests
+# pytest -s -v ~/seleniumPy/seleniumPython/test_3_5_4_markSkip.py
+# the result will be: 1 passed, 1 skipped
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    print("\nquit browser..")
+    yield browser
+    browser.quit()
+
+
+class TestMainPage1():
+
+    @pytest.mark.skip
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
