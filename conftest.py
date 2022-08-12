b043e9c606ec662080b2cc77c12203054a6e566d
Victor Stadnichenko <vstadnichenko@gmail.com>
2022-08-12 16:52:31 +0300

    parametrization

---
+++ conftest.py
@@ -0,0 +1,41 @@
+# the location of this file is: top_directory/conftest.py
+# the fixture to start chrome driver for tests
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    print("\nquit browser..")
+    yield browser
+    browser.quit()
+
+
+# the fixture to choose between chrome and gecko driver for tests
+
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ test3_6_4conftest.py
@@ -0,0 +1,11 @@
+# this shows how the test file accesses the fixture "browser" in file conftest.py
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
